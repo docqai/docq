d92fcba83af4b0e4a6e11bfe12a2b101a7744961
Janaka Abeywardhana <contact@janaka.co.uk>
2023-06-04 17:01:59 +0100

    chore: DOCQ_DATA folder check
    Check if the folder configured exists
    if not attempt to create it.
    
    Call _config_logging() first so we so it applies in all following calls.

--- source/docq/setup.py
+++ source/docq/setup.py
@@ -7,6 +7,7 @@ def _load_env_vars():
     for k in ['DOCQ_DATA', 'OPENAI_API_KEY']:
         if k not in os.environ:
             if k in st.secrets:
+                logging.debug(f'{k} not found as environment variable. Loading from secrets.toml.')
                 os.environ[k] = st.secrets[k]
             else:
                 logging.error(f'{k} not set as environment variable or in secrets.toml.')
@@ -14,8 +15,13 @@ def _load_env_vars():
 def _config_logging():
     logging.basicConfig(level=logging.DEBUG)
 
+def _setup_persistance_folder():    
+    if not os.path.exists(os.environ["DOCQ_DATA"]):
+        os.makedirs(os.environ["DOCQ_DATA"])
+        logging.info(f'Created folder `{os.environ["DOCQ_DATA"]}` because it didn\'t exist.')
 
 def init():
-    _load_env_vars()
     _config_logging()
+    _load_env_vars()
+    _setup_persistance_folder()
     logging.info("Docq initialized with env vars: %s", os.environ)
