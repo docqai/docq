89f8fc7f42a359fc4d2c95da339c08e83196e191
Janaka Abeywardhana <contact@janaka.co.uk>
2023-06-05 00:11:41 +0100

    refactor: remove persisted folder setup
    duplicated, following rebase. handled elsewhere

--- source/docq/setup.py
+++ source/docq/setup.py
@@ -13,22 +13,13 @@ def _load_env_vars():
                 logging.debug(f'{k} not found as environment variable. Loading from secrets.toml.')
                 os.environ[k] = st.secrets[k]
             else:
-                logging.error(f'{k} not set as environment variable or in secrets.toml. dfg')
+                logging.error(f'{k} not set as environment variable or in secrets.toml.')
 
 def _config_logging():
     logging.basicConfig(level=logging.DEBUG)
-
-def _setup_persistance_folder():
-    try:
-        if not os.path.exists(os.environ["DOCQ_DATA"]):
-            os.makedirs(os.environ["DOCQ_DATA"])
-            logging.info(f'Created folder `{os.environ["DOCQ_DATA"]}` because it didn\'t exist.')
-    except OSError as e:
-        logging.error(f'Error creating folder `{os.environ["DOCQ_DATA"]}`: {e}')
 
 def init():
     _config_logging()
     _load_env_vars()
     _init_admin_if_necessary()
-    _setup_persistance_folder()
     logging.info("Docq initialized")
