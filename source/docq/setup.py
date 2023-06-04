39dbdb49d01fee68f1b9885361d6dc8a93db263c
Janaka Abeywardhana <contact@janaka.co.uk>
2023-06-04 16:34:10 +0100

    chore: add support for secrets env vars
    secrets set as env vars will override values in a secrets.toml.
    secrets.toml is only checked if an env var isn't present.

--- source/docq/setup.py
+++ source/docq/setup.py
@@ -2,12 +2,14 @@ import os
 import logging
 import streamlit as st
 
-
 def _load_env_vars():
-    for k in ['DOCQ_DATA', 'OPENAI_API_KEY']:
-        if k in st.secrets:
-            os.environ[k] = st.secrets[k]
 
+    for k in ['DOCQ_DATA', 'OPENAI_API_KEY']:
+        if k not in os.environ:
+            if k in st.secrets:
+                os.environ[k] = st.secrets[k]
+            else:
+                logging.error(f'{k} not set as environment variable or in secrets.toml.')
 
 def _config_logging():
     logging.basicConfig(level=logging.DEBUG)
