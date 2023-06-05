f9cb734bd85b6721d32c1ed1f54e47e46aca4453
Janaka Abeywardhana <contact@janaka.co.uk>
2023-06-05 13:05:04 +0100

    refactor: remove env var load function
    it's not needed. Streamlit autobinds secrets.toml to env vars
    if the file is present.

--- source/docq/setup.py
+++ source/docq/setup.py
@@ -5,21 +5,10 @@ import streamlit as st
 
 from .manage_users import _init_admin_if_necessary
 
-def _load_env_vars():
-
-    for k in ['DOCQ_DATA', 'OPENAI_API_KEY']:
-        if k not in os.environ:
-            if k in st.secrets:
-                logging.debug(f'{k} not found as environment variable. Loading from secrets.toml.')
-                os.environ[k] = st.secrets[k]
-            else:
-                logging.error(f'{k} not set as environment variable or in secrets.toml.')
-
 def _config_logging():
     logging.basicConfig(level=logging.DEBUG)
 
 def init():
     _config_logging()
-    _load_env_vars()
     _init_admin_if_necessary()
     logging.info("Docq initialized")
