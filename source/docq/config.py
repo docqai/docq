384f3b6a37dddd47925aeb4fda29c3f32e91f915
Chen Wang <dev@chenwang.org>
2023-06-30 10:43:04 +0100

    Use document metadata during (re)indexing

--- source/docq/config.py
+++ source/docq/config.py
@@ -3,6 +3,7 @@
 from enum import Enum
 
 ENV_VAR_DOCQ_DATA = "DOCQ_DATA"
+ENV_VAR_DOCQ_DEMO = "DOCQ_DEMO"
 ENV_VAR_OPENAI_API_KEY = "OPENAI_API_KEY"
 
 
@@ -10,6 +11,14 @@ LLM_MODELS = {
     "OPENAI_CHAT": (["gpt-3.5-turbo", "gpt-4"], range(0, 2)),
     "OPENAI": (["text-davinci-003", "text-davinci-002", "code-davinci-002"], range(0, 2)),
 }
+
+
+class DocumentMetadata(Enum):
+    """Document metadata."""
+
+    FILE_PATH = "file_path"
+    SPACE_ID = "space_id"
+    SPACE_TYPE = "space_type"
 
 
 class SpaceType(Enum):
