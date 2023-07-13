6f2807259ea4ef97b60c0e3f8c1a4c936a8c93be
Janaka Abeywardhana <contact@janaka.co.uk>
2023-07-13 19:43:19 +0100

    improve format_filesize to handle smaller sizes
    so it doesn't show as zero

--- web/utils/formatters.py
+++ web/utils/formatters.py
@@ -10,9 +10,9 @@ def format_datetime(dt: datetime) -> str:
 
 def format_filesize(size: int) -> str:
     """Format filesize to human-friendly Mb sizing."""
-    return f"{round(size / 1024 / 1024, 1)} Mb"
-
-
-def format_archived(text: str, archived: bool = True) -> str:
-    """Format a line of text depending on whether it's achived or not."""
-    return f"{'~~' if archived else ''}{text}{'~~' if archived else ''}"
+    if size > 1024 * 1024:
+        return f"{round(size / 1024 / 1024, 1)} Mb"
+    elif size > 1024:
+        return f"{round(size / 1024 , 1)} Kb"
+    else:
+        return f"{size} bytes"
