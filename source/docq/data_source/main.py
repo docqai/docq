e9f7556776494510e50daf0b6f6b2ca921953434
Chen Wang <dev@chenwang.org>
2023-07-17 13:37:05 +0100

    Fix a few issues

--- source/docq/data_source/main.py
+++ source/docq/data_source/main.py
@@ -1,7 +1,6 @@
 """Data source for Docq."""
 
 from abc import ABC, abstractmethod
-from dataclasses import dataclass, field
 from enum import Enum
 from typing import List
 
@@ -13,12 +12,12 @@ from ..domain import ConfigKey, SpaceKey
 class DocumentMetadata(Enum):
     """Document metadata."""
 
-    FILE_PATH = "file_path"
-    SPACE_ID = "space_id"
-    SPACE_TYPE = "space_type"
-    DATA_SOURCE_TYPE = "data_source_type"
-    INDEXED_ON = "indexed_on"
-    SOURCE_URI = "source_url"
+    FILE_PATH = "File Path"
+    SPACE_ID = "Space ID"
+    SPACE_TYPE = "Space Type"
+    DATA_SOURCE_TYPE = "Data Source Type"
+    INDEXED_ON = "Indexed Timestamp"
+    SOURCE_URI = "Source URI"
 
 
 class SpaceDataSource(ABC):
@@ -60,19 +59,5 @@ class SpaceDataSourceFileBased(SpaceDataSource):
         pass
 
 
-class SpaceDataSourceWebBased(SpaceDataSource):
+class SpaceDataSourceWebBased(SpaceDataSourceFileBased):
     """Abstract definition of a file-based data source for a space. To be extended by concrete data sources."""
-
-    @abstractmethod
-    def get_document_list(self, space: SpaceKey, configs: dict, persist_path: str) -> list[tuple[str, int, int]]:
-        """Returns a list of tuples containing the name, creation time, and size (Mb) of each document in the specified space's cnfigured data source.
-
-        Args:
-            space (SpaceKey): The space to retrieve the document list for.
-            configs (dict): A dictionary of configuration options.
-            persist_path (str): The path to persist the downloaded document list.
-
-        Returns:
-            list[tuple[str, int, int]]: A list of tuples containing the name, creation time, and size of each document in the specified space's upload directory.
-        """
-        pass
