4ac8e1248dff3c043194fd1e7da600663c76098c
Jashon Osala <jashonosala@gmail.com>
2023-07-20 20:28:34 +0300

    chore: Group pages under site for web sources

--- source/docq/manage_documents.py
+++ source/docq/manage_documents.py
@@ -87,15 +87,23 @@ def _group_sources(page: str, filename: str, group: dict[str, list[str]] = {}) -
         group[filename] = [page]
     return group
 
+def _group_web_source(site:str, title: str, page: str, group: dict[str, list[tuple[str, str]]] = {}) -> dict[str, list[tuple[str, str]]]:  # noqa: B006
+    if site in group:
+        group[site].append((title, page))
+    else:
+        group[site] = [(title, page)]
+    return group
+
 def format_document_sources(source_nodes: list[NodeWithScore]) -> str:
     """Return the formatted sources with a clickable download link."""
     try:
         delimiter = "\n\n"
+        site_delimiter = "\n>- "
         _sources = []
         source_groups: dict[str, list[str]] = {}
         source_uri: dict[str, str] = {}
         source_types: dict[str, str] = {}
-        web_sources: dict[str, tuple[str, str]] = {}
+        web_sources: dict[str, list[tuple[str, str]]] = {}
 
         for source_node in source_nodes:
             metadata = dict(source_node.node.metadata,  **source_node.node.extra_info)
@@ -110,7 +118,7 @@ def format_document_sources(source_nodes: list[NodeWithScore]) -> str:
                     source_uri[name] = uri
                     source_types[name] = s_type
                     continue
-                web_sources[page_label] = name, uri
+                web_sources = _group_web_source(name, page_label, uri, web_sources)
 
         for name, page_labels in source_groups.items():
             uri = source_uri.get(name)
@@ -118,9 +126,9 @@ def format_document_sources(source_nodes: list[NodeWithScore]) -> str:
             if uri:
                 download_url = _get_download_link(name, uri) if source_type == "Manual Upload" else uri
                 _sources.append(f"> *File:* [{name}]({download_url})<br> *Pages:* {', '.join(page_labels)}")
-        for page_title, links in web_sources.items():
-            name, page_url = links
-            _sources.append(f"> *Site:* {name}<br>*Page:* [{page_title}]({page_url})")
+        for site, pages in web_sources.items():
+            page_str = site_delimiter.join([f"[{title}]({page})" for title, page in pages])
+            _sources.append(f"> {site} {site_delimiter if page_str else ''}{page_str}")
         return delimiter.join(_sources)
 
     except Exception as e:
