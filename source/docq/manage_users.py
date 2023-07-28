4f72d65d722a37f2317e814eb282e506cc453c73
Jashon Osala <jashonosala@gmail.com>
2023-07-29 00:19:16 +0300

    chore: Update imports

--- source/docq/manage_users.py
+++ source/docq/manage_users.py
@@ -15,7 +15,6 @@ from .config import SpaceType
 from .domain import SpaceKey
 from .support.auth_utils import cache_auth
 from .support.store import get_sqlite_system_file
-from .support.utils import cache_auth
 
 SQL_CREATE_USERS_TABLE = """
 CREATE TABLE IF NOT EXISTS users (
