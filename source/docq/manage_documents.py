c0a1f93cb13b12743058553743d12499632f4856
Jashon Osala <jashonosala@gmail.com>
2023-08-01 09:49:03 +0300

    chore: Specify datasources using type

--- source/docq/manage_documents.py
+++ source/docq/manage_documents.py
@@ -10,7 +10,6 @@ from typing import Any, Dict
 from llama_index.schema import NodeWithScore
 from streamlit import runtime
 
-from .data_source.list import SpaceDataSources
 from .data_source.main import DocumentMetadata
 from .domain import SpaceKey
 from .manage_spaces import reindex
@@ -47,7 +46,7 @@ def delete_all(space: SpaceKey) -> None:
 
 def _get_download_link(filename: str, path: str) -> str:
     """Return the download link for the file if runtime exists, otherwise return an empty string."""
-    if runtime.exists():
+    if runtime.exists() and os.path.isfile(path):
         return runtime.get_instance().media_file_mgr.add(
             path_or_data=path,
             mimetype=guess_type(path)[0] or "application/octet-stream",
@@ -57,29 +56,33 @@ def _get_download_link(filename: str, path: str) -> str:
         )
 
     else:
-        return ""
+        return "#"
+
+def _remove_ascii_control_characters(s: str) -> str:
+    """Remove ASCII control characters from string."""
+    for i in range(32):
+        s = s.replace(chr(i), "")
+    return s
 
 
 def _parse_metadata(metadata: Dict[str, Any]) -> tuple[Any, Any, Any, Any] | None:
     if not metadata:
         return None
-    s_name = metadata.get(str(DocumentMetadata.DATA_SOURCE_NAME.name).lower())
-    if s_name == SpaceDataSources.WEB_SCRAPER.value.get_name() or s_name ==  SpaceDataSources.KNOWLEDGE_BASE_SCRAPER.value.get_name():
+    s_type = metadata.get(str(DocumentMetadata.DATA_SOURCE_TYPE.name).lower())
+    if s_type == 'SpaceDataSourceWebBased':
         website = metadata.get("source_website")
         page_url = metadata.get(str(DocumentMetadata.SOURCE_URI.name).lower())
-        page_title = metadata.get("page_title")
-        if ord(str(page_title)[-1]) < 32 or ord(str(page_title)[-1]) > 126:
-            page_title = str(page_title)[:-1]
+        page_title = _remove_ascii_control_characters(metadata.get("page_title"))
 
         if not website or not page_url:
             return None
-        return page_url, page_title, website, s_name
+        return page_url, page_title, website, s_type
     uri = metadata.get(str(DocumentMetadata.SOURCE_URI.name).lower())
     if not uri:
         return None
     page_label = metadata.get("page_label")
     file_name = metadata.get("file_name")
-    return uri, page_label, file_name, s_name
+    return uri, page_label, file_name, s_type
 
 def _group_sources(page: str, filename: str, group: dict[str, list[str]] = {}) -> dict[str, list[str]]:  # noqa: B006
     if filename in group:
@@ -103,28 +106,29 @@ def format_document_sources(source_nodes: list[NodeWithScore]) -> str:
         _sources = []
         source_groups: dict[str, list[str]] = {}
         source_uri: dict[str, str] = {}
-        source_names: dict[str, str] = {}
+        souce_types: dict[str, str] = {}
         web_sources: dict[str, list[tuple[str, str]]] = {}
 
         for source_node in source_nodes:
             data = _parse_metadata(source_node.node.metadata)
+            print(f"\x1b[1;32mDebug metadata{source_node.node.metadata}\x1b[0m")
             if data is not None:
-                uri, page_label, name, s_name = data
-                if not uri or not name or not s_name:
+                uri, page_label, name, s_type = data
+                if not uri or not name or not s_type:
                     continue
-                scraper = s_name == SpaceDataSources.WEB_SCRAPER.value.get_name() or s_name ==  SpaceDataSources.KNOWLEDGE_BASE_SCRAPER.value.get_name()
+                scraper = s_type == 'SpaceDataSourceWebBased'
                 if not scraper:
                     source_groups = _group_sources(page_label, name, source_groups)
                     source_uri[name] = uri
-                    source_names[name] = s_name
+                    souce_types[name] = s_type
                     continue
                 web_sources = _group_web_source(name, page_label, uri, web_sources)
 
         for name, page_labels in source_groups.items():
             uri = source_uri.get(name)
-            source_name = source_names.get(name)
+            source_type = souce_types.get(name)
             if uri:
-                download_url = _get_download_link(name, uri) if source_name == SpaceDataSources.MANUAL_UPLOAD.value.get_name() else uri
+                download_url = _get_download_link(name, uri) if source_type == 'SpaceDataSourceFileBased' else uri
                 _sources.append(f"> *File:* [{name}]({download_url})<br> *Pages:* {', '.join(page_labels)}")
         for site, pages in web_sources.items():
             page_str = site_delimiter.join([f"[{title}]({page})" for title, page in pages])
