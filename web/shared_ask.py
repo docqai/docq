2c219687b50540a988eb7cc44a2e8ab2df748c23
Jashon Osala <jashonosala@gmail.com>
2023-08-12 09:25:18 +0300

    chore: Cleanup files.

--- web/shared_ask.py
+++ web/shared_ask.py
@@ -3,7 +3,7 @@
 from docq.config import FeatureType
 from docq.domain import FeatureKey
 from st_pages import add_page_title
-from utils.layout import ask_shared_layout_style, auth_required, chat_ui, feature_enabled
+from utils.layout import auth_required, chat_ui, feature_enabled
 from utils.sessions import get_authenticated_user_id
 
 auth_required()
