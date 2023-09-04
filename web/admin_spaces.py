5048f6667a0796044c67712b2f4ec461c1aa90d5
Jashon Osala <64925863+osala-eng@users.noreply.github.com>
2023-09-04 20:42:31 +0300

    Ux/consolidate space mgmt (#89)
    * chore: Re-arrange admin docs layout.
    
    * chore: Update admin docs UI layout.
    
    * chore: Rename consolidated space to Admin Spaces.
    
    * Enhancement/make selector sticky (#84)
    
    * feat: Make the space selector box stick to the top of the page.
    
    * chore: Add custom styling for personal ask.
    
    * chore: Remove unused imports.
    
    * chore: Add an avatar to chat messages
    
    * chore: Added script to format avatars.
    
    * feat: Make the space selector box stick to the top of the page.
    
    * chore: Remove unused imports.
    
    * Update user avatar settings.
    
    * chore: Attach event listeners to space selector.
    
    * chore: Cleanup imports.
    
    * chore: Update chat_ui script.
    
    * feat: Make the space selector box stick to the top of the page.
    
    * chore: Use username as email for gravatar requests.
    
    * chore: Use identicon as avatar default
    
    * chore: Set username in auth session state.
    
    * chore: Bump llama-index to 0.8.5.post2
    
    * Update get username function

--- web/admin_spaces.py
+++ web/admin_spaces.py
@@ -1,11 +1,16 @@
-"""Page: Admin / Manage Spaces."""
-
+"""Page: Admin / Manage Documents."""
+import streamlit as st
 from st_pages import add_page_title
-from utils.layout import auth_required, create_space_ui, list_spaces_ui
+from utils.layout import admin_docs_ui, auth_required, create_space_ui
 
 auth_required(requiring_admin=True)
 
 add_page_title()
 
+PARAM_NAME = "sid"
+st.session_state["admin_docs_alert"] = st.empty()
+
+## Create Space UI
 create_space_ui()
-list_spaces_ui(True)
+
+admin_docs_ui(PARAM_NAME)
