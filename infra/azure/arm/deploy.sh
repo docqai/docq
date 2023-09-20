915762ccd86ff70c52b28bd551ca48284b18af5d
Janaka Abeywardhana <contact@janaka.co.uk>
2023-09-20 14:50:32 +0100

    Infra: update Azure OpenAI models (#107)
    text-davinci is deprecated now and breaks the Arm template.
    
    `gpt-35-turbo` version 2 is the recommended for chat and generation.
    
    adding `text-embedding-ada-002` instead of davinci for embedding as per recommendation

--- infra/azure/arm/deploy.sh
+++ infra/azure/arm/deploy.sh
@@ -5,29 +5,52 @@ LOCATION="${2:-"westeurope"}"
 
 RESOURCE_GROUP="${NAME}-rg-${LOCATION}"
 
-
+echo "Resourse Group Create response:"
 res1=$(az group create --name $RESOURCE_GROUP --location $LOCATION)
+
 echo $res1 | jq '.'
+status1=$(echo $res1 | (grep -E -o -i -w '"provisioningState": "Succeeded"') | head -1)
+echo "status=${status1}"
 
-echo "Resource group '${RESOURCE_GROUP}' created."
 
-res2=$(az deployment group create --resource-group $RESOURCE_GROUP --name $NAME --template-file appservice.json --parameters @appservice.parameters.json)
-
-status=$(echo $res2 | (grep -E -o 'Failed' || echo 'Success') | head -1)
-
-echo $res2 | jq '.'
-
-echo "status=${status}"
-echo $status
-
-if [ $status = "Failed" ]
+if [ -n "$status1" ]
 then  
+  echo "Outputs:"
+  echo "Resource group '${RESOURCE_GROUP}' created."
+else
+  echo "Resource group '${RESOURCE_GROUP}' creation failed!"
   echo "Deployment failed."
   exit 1
-else
-  echo "Outputs:"
-  echo $res2 | jq -r '.properties.outputs'
-  echo "Docq stack successfully deployed."
-  exit 0
 fi
 
+
+echo "Deployment Group Create response:"
+echo "---"
+res2=$(az deployment group create --resource-group $RESOURCE_GROUP --name $NAME --template-file appservice.json --parameters @appservice.parameters.json)
+echo "---"
+status2=$(echo $res2 | (grep -E -o -i -w '"provisioningState": "Succeeded"') | head -1)
+
+echo "---"
+echo $res2 | jq '.'
+echo "---"
+echo "status=${status2}"
+
+if [ -n "$status2" ]
+then  
+  echo "Outputs:"
+  echo $res2 | jq -r '.properties.outputs'
+  echo "Deployment group creation completed!"
+else
+  echo "Deployment group creation failed!"
+  echo "Deployment failed."
+  exit 1
+fi
+
+if [ -n "$status1" ] && [ -n "$status2" ]
+then  
+  echo "Docq stack successfully deployed."
+  exit 0
+else
+  echo "Deployment failed."
+  exit 1
+fi
