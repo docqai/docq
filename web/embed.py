3e85144f87b98fcb061626ed5a41b212221313bb
Jashon Osala <jashonosala@gmail.com>
2023-09-20 13:48:07 +0300

    chore: Update web embed to use ASK_PUBLIC feature.

--- web/embed.py
+++ web/embed.py
@@ -6,8 +6,8 @@ from utils.sessions import get_active_public_session
 
 auth_required(show_login_form=False, show_logout_button=False)
 
-public_space_enabled(FeatureType.ASK_SHARED)
+public_space_enabled(FeatureType.ASK_PUBLIC)
 
-feature = FeatureKey(FeatureType.ASK_SHARED, get_active_public_session())
+feature = FeatureKey(FeatureType.ASK_PUBLIC, get_active_public_session())
 
 chat_ui(feature)
