82bd8a53da62cbcd82824695b0ad9620a63afce9
Jashon Osala <64925863+osala-eng@users.noreply.github.com>
2023-09-27 16:55:55 +0300

    Bug/error when switching spaces (#110)
    * fix: Fix error when switching spaces in admin docs.
    
    * chore: Move page web/admin_docs.py to web/admin_spaces.

--- web/admin_spaces.py
+++ web/admin_spaces.py
@@ -1,5 +1,4 @@
 """Page: Admin / Manage Documents."""
-import streamlit as st
 from st_pages import add_page_title
 from utils.layout import admin_docs_ui, auth_required, create_space_ui
 
@@ -8,7 +7,6 @@ auth_required(requiring_admin=True)
 add_page_title()
 
 PARAM_NAME = "sid"
-st.session_state["admin_docs_alert"] = st.empty()
 
 ## Create Space UI
 create_space_ui()
