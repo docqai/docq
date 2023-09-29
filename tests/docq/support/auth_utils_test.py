5c4fefccb10bb0102039503863ee55a20eeb7928
Janaka Abeywardhana <contact@janaka.co.uk>
2023-09-29 14:25:38 +0100

    tests: fix auth utils tests after refactor

--- tests/docq/support/auth_utils_test.py
+++ tests/docq/support/auth_utils_test.py
@@ -4,26 +4,24 @@ from secrets import token_hex
 from typing import Self
 from unittest.mock import Mock, patch
 
-from docq.config import FeatureType
 from docq.support import auth_utils
 from docq.support.auth_utils import (
-    _auto_login_enabled,
+    SESSION_COOKIE_NAME,
     _clear_cookie,
     _create_hmac,
-    _decrypt_auth,
-    _encrypt_auth,
-    _get_cookies,
+    _decrypt,
+    _encrypt,
     _get_cookie_session_id,
+    _get_cookies,
     _set_cookie,
     _set_cookie_session_id,
     _verify_hmac,
-    set_cache_auth_session,
     cached_sessions,
     generate_hmac_session_id,
     get_cache_auth_session,
-    session_data,
     reset_cache_and_cookie_auth_session,
-    SESSION_COOKIE_NAME,
+    session_data,
+    set_cache_auth_session,
 )
 
 
@@ -80,51 +78,51 @@ class TestAuthUtils(unittest.TestCase):
         mock_set_cookie.assert_called_once_with(session_id)
 
     @patch("docq.support.auth_utils._get_cookies")
-    def test_get_session_id(self: Self, mock_get_cookies: Mock) -> None:
+    def test_get_cookie_session_id(self: Self, mock_get_cookies: Mock) -> None:
         """Test get session id."""
         session_id = generate_hmac_session_id()
-        cached_sessions[session_id] = _encrypt_auth(("9999", "user", 1))
-        mock_get_cookies.return_value = {"docqai/_docq": session_id}
+        cached_sessions[session_id] = _encrypt(("9999", "user", 1))
+        mock_get_cookies.return_value = {SESSION_COOKIE_NAME: session_id}
         result = _get_cookie_session_id()
         assert result == session_id
 
     def test_encrypt_decrypt_auth(self: Self) -> None:
         """Test encrypt decrypt auth."""
-        auth, kwargs = ("9999", "user", 1), {}
-        encrypted_auth = _encrypt_auth(*auth, **kwargs)
-        decrypted_auth = _decrypt_auth(encrypted_auth)
-        assert (auth, kwargs) == decrypted_auth
+        payload = {"org_id": "9999", "username": "user name", "user_id": 1}
+        encrypted_auth = _encrypt(payload)
+        decrypted_auth = _decrypt(encrypted_auth)
+        assert payload == decrypted_auth
 
-    @patch("docq.support.auth_utils._get_session_id")
-    @patch("docq.support.auth_utils._auto_login_enabled")
-    def test_cache_auth(self: Self, mock_auto_login_enabled: Mock, mock_get_session_id: Mock) -> None:
+    @patch("docq.support.auth_utils._get_cookie_session_id")
+    def test_cache_auth(self: Self, mock_get_cookie_session_id: Mock) -> None:
         """Test cache auth."""
-        args, kwargs = ("9999", "user", 1), {}
+        payload = {"org_id": "9999", "username": "user name", "user_id": 1}
         session_id = generate_hmac_session_id()
-        mock_get_session_id.return_value = session_id
-        mock_auto_login_enabled.return_value = True
-        set_cache_auth_session(*args, **kwargs)
+        mock_get_cookie_session_id.return_value = session_id
+        set_cache_auth_session(payload)
         assert session_id in cached_sessions
 
-    @patch("docq.support.auth_utils._auto_login_enabled")
-    @patch("docq.support.auth_utils._get_session_id")
-    def test_auth_result(self: Self, mock_get_session_id: Mock, mock_auto_login_enabled: Mock) -> None:
+    @patch("docq.support.auth_utils._get_cookie_session_id")
+    def test_auth_result(
+        self: Self,
+        mock_get_cookie_session_id: Mock,
+    ) -> None:
         """Test auth result."""
-        args, kwargs = (("9999", "user", 1), {})
+        payload = {"org_id": "9999", "username": "user name", "user_id": 1}
         session_id = generate_hmac_session_id()
-        mock_get_session_id.return_value = session_id
-        mock_auto_login_enabled.return_value = True
-        set_cache_auth_session(*args, **kwargs)
+        mock_get_cookie_session_id.return_value = session_id
+        # mock_auto_login_enabled.return_value = True
+        set_cache_auth_session(payload)
         result = get_cache_auth_session()
-        assert result == (args, kwargs), "Auth result should be same as input"
+        assert result == {"org_id": "9999", "username": "user name", "user_id": 1}
 
-    @patch("docq.support.auth_utils._get_session_id")
-    def test_session_logout(self: Self, mock_get_session_id: Mock) -> None:
+    @patch("docq.support.auth_utils._get_cookie_session_id")
+    def test_session_logout(self: Self, mock_get_cookie_session_id: Mock) -> None:
         """Test session logout."""
         session_id = generate_hmac_session_id()
-        cached_sessions[session_id] = _encrypt_auth(("9999", "user", 1))
+        cached_sessions[session_id] = _encrypt(("9999", "user", 1))
         session_data[session_id] = session_id
-        mock_get_session_id.return_value = session_id
+        mock_get_cookie_session_id.return_value = session_id
         reset_cache_and_cookie_auth_session()
         assert session_id not in cached_sessions, "Cached session should be deleted on logout"
         assert session_id not in session_data, "Session data should be deleted on logout"
