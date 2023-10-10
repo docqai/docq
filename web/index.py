1b021ca5db909118ddbcd67449acd342a59df130
Jashon Osala <jashonosala@gmail.com>
2023-10-10 12:24:10 +0300

    feat: Signup page layout.

--- web/index.py
+++ web/index.py
@@ -15,6 +15,7 @@ with st.sidebar:
 show_pages(
     [
         Page("web/index.py", "Home", "🏠"),
+        Page("web/signup.py", "Signup"),
         Section("Your_Space", icon="📁"),
         Page("web/personal_chat.py", "General_Chat"),
         Page("web/personal_ask.py", "Ask_Your_Documents"),
