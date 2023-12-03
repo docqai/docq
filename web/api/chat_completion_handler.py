1eb9b3e2d02cda1516b550e7a15b77103f347756
Janaka Abeywardhana <contact@janaka.co.uk>
2023-12-03 20:20:38 +0530

    feat: add first web API REST endpoints (alpha) (#171)
    * Add REST API framework implementation into Streamlit (alpha)
      * Uses a hack to get a reference to the Tornado web server instance to add routes + handlers.
      * Has token-based auth hooks wired up. Token gen and checking need to be implemented.
      * Pydantic models to handle request/response payloads (de)serialisation.
      * HTTP error handling based on Pydantic validation
    
    * Add endpoints (alpha):
      * /api/chat/completion - direct LLM calls
      * /api/rag/completion - RAG based on spaces
    
    * bump version v0.7.0 -> v0.7.1

---
+++ web/api/chat_completion_handler.py
@@ -0,0 +1,68 @@
+"""Handle /api/chat/completion requests."""
+import json
+from typing import Optional, Self
+
+from docq.model_selection.main import get_model_settings_collection
+from docq.run_queries import run_chat
+from py import log
+from pydantic import BaseModel, Field, ValidationError
+from tornado.web import HTTPError, RequestHandler
+
+from web.api.utils import CamelModel, authenticated
+from web.utils.streamlit_application import st_app
+
+
+class PostRequestModel(CamelModel):
+    """Pydantic model for the request body."""
+    input_: str = Field(..., alias="input")
+    history: Optional[str] = Field(None)
+    model_settings_collection_name: Optional[str] = Field(None)
+
+class PostResponseModel(CamelModel):
+    """Pydantic model for the response body."""
+    response: str
+    meta: Optional[dict[str,str]] = None
+
+@st_app.api_route("/api/chat/completion")
+class ChatCompletionHandler(RequestHandler):
+    """Handle /api/chat/completion requests."""
+
+    def check_origin(self: Self, origin) -> bool:
+        """Override the origin check if it's causing problems."""
+        return True
+
+    def check_xsrf_cookie(self) -> bool:
+        # If `True`, POST, PUT, and DELETE are block unless the `_xsrf` cookie is set.
+        # Safe with token based authN
+        return False
+
+    def get(self: Self) -> None:
+        """Handle GET request."""
+        self.write({"message": "hello world 2"})
+
+
+
+    @authenticated
+    def post(self: Self) -> None:
+        """Handle POST request.
+
+        Example:
+        ```shell
+        curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer expected_token" -d /
+        '{"input":"what's the sun?", "modelSettingsCollectionName"}' http://localhost:8501/api/chat/completion
+        ```
+        """
+        body = self.request.body
+
+        try:
+            request_model = PostRequestModel.model_validate_json(body)
+            history = request_model.history if request_model.history else ""
+            model_usage_settings = get_model_settings_collection(request_model.model_settings_collection_name) if request_model.model_settings_collection_name else get_model_settings_collection("azure_openai_latest")
+            result = run_chat(input_=request_model.input_, history=history, model_settings_collection=model_usage_settings)
+            response_model = PostResponseModel(response=result.response, meta={"model_settings": model_usage_settings.key})
+
+            self.write(response_model.model_dump())
+
+        except ValidationError as e:
+            raise HTTPError(status_code=400, reason="Invalid request body", log_message=str(e)) from e
+
---
+++ web/api/rag_completion_handler.py
@@ -0,0 +1,77 @@
+"""Handle /api/rag/completion requests."""
+import json
+from typing import Optional, Self
+
+from docq.domain import SpaceKey, SpaceType
+from docq.manage_spaces import list_public_spaces
+from docq.model_selection.main import get_model_settings_collection
+from docq.support.llm import run_ask
+from llama_index import Response
+from llama_index.response.schema import PydanticResponse
+from pydantic import Field, ValidationError
+from tornado.web import HTTPError, RequestHandler
+
+from web.utils.streamlit_application import st_app
+
+from .utils import CamelModel, authenticated
+
+
+class PostRequestModel(CamelModel):
+    """Pydantic model for the request body."""
+    input_: str = Field(..., alias="input")
+    history: Optional[str] = None
+    space_group_id: Optional[int] = Field(None)
+    org_id: Optional[int] = Field(None)
+    model_settings_collection_name: Optional[str] = Field(None)
+
+
+class PostResponseModel(CamelModel):
+    """Pydantic model for the response body."""
+    response: str
+
+
+@st_app.api_route("/api/rag/completion")
+class RagCompletionHandler(RequestHandler):
+    """Handle /api/rag/completion requests."""
+
+    def check_origin(self: Self, origin) -> bool:
+        """Override the origin check if it's causing problems."""
+        return True
+
+    def check_xsrf_cookie(self) -> bool:
+        # If `True`, POST, PUT, and DELETE are block unless the `_xsrf` cookie is set.
+        # Safe with token based authN
+        return False
+
+    @authenticated
+    def post(self: Self) -> None:
+        """Handle POST request."""
+        body = self.request.body
+        try:
+            request_model = PostRequestModel.model_validate_json(body)
+
+            space_keys: list[SpaceKey] = []
+            if request_model.space_group_id:
+                spaces = list_public_spaces(request_model.space_group_id)
+                for s in spaces:
+                    space_keys.append(SpaceKey(id_ = s[0], type_=SpaceType.PUBLIC, org_id=request_model.org_id if request_model.org_id else 0, summary=s[3]))
+
+            try:
+                model_usage_settings = get_model_settings_collection(request_model.model_settings_collection_name) if request_model.model_settings_collection_name else get_model_settings_collection("azure_openai_latest")
+            except ValueError as e:
+                raise HTTPError(400, "Invalid modelSettingsCollectionName") from e
+
+            history = request_model.history if request_model.history else ""
+            result = run_ask(input_=request_model.input_, history=history, model_settings_collection=model_usage_settings)
+
+            if result:
+                if isinstance(result, Response) and result.response:
+                    response_model = PostResponseModel(response=result.response)
+                    # Dump the model to a dictionary
+                    self.write(response_model.model_dump())
+                else:
+                    self.write({"error": "Response type not supported by web API"})
+        except ValidationError as e:
+            raise HTTPError(400, "Invalid request body") from e
+
+
