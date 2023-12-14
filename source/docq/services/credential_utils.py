71d8c795e347eb44c9843fad953b20b0585751c8
Janaka Abeywardhana <contact@janaka.co.uk>
2023-12-14 13:26:53 +0530

    refactor: GCP env var name

--- source/docq/services/credential_utils.py
+++ source/docq/services/credential_utils.py
@@ -10,24 +10,28 @@ from typing import Optional
 
 from opentelemetry import trace
 
+from .google_drive import CREDENTIALS_KEY
+
 tracer = trace.get_tracer(__name__)
 
 
-def load_gcp_credentials(save_path: Optional[str] = None) -> bool:
-    """Returns a GCP credentials object.
+def load_gcp_credentials_from_env_var(save_path: Optional[str] = None) -> bool:
+    """Saves credentials json from an env var to a file for services that only accept a file path.
 
     Args:
         save_path (str): Path inc filename to save the credentials JSON to. Default `./.streamlit/gcp_credentials.json`
     """
     span = trace.get_current_span()
 
+    DOCQ_GOOGLE_APPLICATION_CREDENTIALS_JSON = "DOCQ_GOOGLE_APPLICATION_CREDENTIALS_JSON"  # noqa: N806
+
     success = False
-    credentials_json = "<env var DOCQ_GCP_KEYS_JSON not set>"
+    credentials_json = f"<env var {DOCQ_GOOGLE_APPLICATION_CREDENTIALS_JSON} not set>"
     try:
-        credentials_json = os.environ["DOCQ_GCP_KEYS_JSON"]
+        credentials_json = os.environ[DOCQ_GOOGLE_APPLICATION_CREDENTIALS_JSON] 
     except KeyError as e:
         success = False
-        span.add_event("Failed to access env var DOCQ_GCP_KEYS_JSON", attributes={"error": str(e)})
+        span.add_event(f"Failed to access env var {DOCQ_GOOGLE_APPLICATION_CREDENTIALS_JSON}", attributes={"error": str(e)})
         span.record_exception(e)
 
     path = save_path or "./.streamlit/gcp_credentials.json"
@@ -43,12 +47,13 @@ def load_gcp_credentials(save_path: Optional[str] = None) -> bool:
         span.record_exception(e)
 
     # note: this will only be available to the current process thread. coroutine code will not have access.
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = path
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = path # default GCP env var
+    os.environ[CREDENTIALS_KEY] = path # used by the google_drive.py module.
 
     return success
 
 def setup_all_service_credentials() -> None:
     """Setup all service credentials."""
     with tracer.start_as_current_span("docq.services.credential_utils.setup_all_service_credentials") as span:
-        load_gcp_credentials()
+        load_gcp_credentials_from_env_var()
         span.add_event("GCP credentials loaded")
