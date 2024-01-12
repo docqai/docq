dffe40fcb6218576b157edd5172fe303a8ba8406
Janaka Abeywardhana <contact@janaka.co.uk>
2024-01-12 12:29:47 +0000

    chore: refactor trace span names
    chore: refactor trace span names

--- source/docq/db_migrations.py
+++ source/docq/db_migrations.py
@@ -48,28 +48,31 @@ def add_space_type_to_spaces_table() -> None:
     """
     with tracer.start_as_current_span("add_space_type_to_spaces_table") as span:
         try:
-            span.add_event("Running migration add_space_type_to_spaces")
-            logging.info("Running migration add_space_type_to_spaces")
+            span.add_event("Running migration add_space_type_to_spaces_table")
+            logging.info("Running migration add_space_type_to_spaces_table")
 
             with closing(sqlite3.connect(get_sqlite_system_file())) as connection, closing(connection.cursor()) as cursor:
                 cursor.execute("SELECT name FROM pragma_table_info('spaces') WHERE name = 'space_type'")
 
                 if cursor.fetchone() is None:
-                    logging.info("db_migrations.add_space_type_to_spaces, space_type column does not exist, adding it")
+                    logging.info("db_migrations.add_space_type_to_spaces_table, space_type column does not exist, adding it")
+                    span.add_event("space_type column does not exist, adding it")
                     try:
                         cursor.execute("BEGIN TRANSACTION")
                         cursor.execute("ALTER TABLE spaces ADD COLUMN space_type TEXT NOT NULL DEFAULT '" + SpaceType.SHARED.name + "'",)
                         connection.commit()
-                        logging.info("db_migrations.add_space_type_to_spaces, space_type column added successfully")
+                        logging.info("db_migrations.add_space_type_to_spaces_table, space_type column added successfully")
+                        span.add_event("space_type column added successfully")
                         span.set_attribute("migration_successful", "true")
 
                     except sqlite3.Error as e:
-                        logging.error("db_migrations.add_space_type_to_spaces, failed to add space_type column to spaces table %s", e)
+                        logging.error("db_migrations.add_space_type_to_spaces_table, failed to add space_type column to spaces table %s", e)
+                        span.set_status(trace.Status(trace.StatusCode.ERROR, "Migration add_space_type_to_spaces_table failed"))
                         span.set_attribute("migration_successful", "false")
                         connection.rollback()
 
         except Exception as e:
-            logging.error("Migration add_space_type_to_spaces failed")
-            span.set_status(trace.Status(trace.StatusCode.ERROR, "Migration add_space_type_to_spaces failed"))
+            logging.error("Migration add_space_type_to_spaces_table failed")
+            span.set_status(trace.Status(trace.StatusCode.ERROR, "Migration add_space_type_to_spaces_table failed"))
             span.record_exception(e)
-            raise Exception("Migration add_space_type_to_spaces failed") from e
+            raise Exception("Migration add_space_type_to_spaces_table failed") from e
