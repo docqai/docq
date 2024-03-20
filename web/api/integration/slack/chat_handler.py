3c28c5b81d7d8b5de29239969e71281ae159bb5c
Jashon Osala <jashonosala@gmail.com>
2024-03-20 19:10:26 +0300

    chore: Acknowledge slack chats and mentions.

--- web/api/integration/slack/chat_handler.py
+++ web/api/integration/slack/chat_handler.py
@@ -14,24 +14,21 @@ CHANNEL_TEMPLATE = "<@{user}> {response}"
 def handle_app_mention(ack: Callable, body: dict, say: Say) -> None:
     """Handle @DocQ App mentions."""
     ack()
-    print(f"\x1b[31mDebug body: {body}\x1b[0m")
-    # response = rag_completion(body["event"]["text"], body["event"]["channel"])
-    # say(
-    #     text=CHANNEL_TEMPLATE.format(user=body["event"]["user"], response=response),
-    #     channel=body["event"]["channel"],
-    #     mrkdwn=True,
-    # )
+    response = rag_completion(body["event"]["text"], body["event"]["channel"])
+    say(
+        text=CHANNEL_TEMPLATE.format(user=body["event"]["user"], response=response),
+        channel=body["event"]["channel"],
+        mrkdwn=True,
+    )
 
 
 @slack_app.event("message")
 def handle_message_im(ack: Callable, body: dict, say: Say) -> None:
     """Handle bot messages."""
-    print(f"\x1b[31mDebug ack: {ack}\x1b[0m")
     ack()
-    print(f"\x1b[31mDebug body: {body}\x1b[0m")
-    # if body["event"]["channel_type"] == "im":
-    #     say(
-    #     text=chat_completion(body["event"]["text"]),
-    #     channel=body["event"]["channel"],
-    #     mrkdwn=True,
-    # )
+    if body["event"]["channel_type"] == "im":
+        say(
+        text=chat_completion(body["event"]["text"]),
+        channel=body["event"]["channel"],
+        mrkdwn=True,
+    )
