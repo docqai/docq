ed7e732b7c48a7be374f3376f3a6146de0114949
Janaka Abeywardhana <contact@janaka.co.uk>
2024-04-05 00:28:43 +0100

    chore: clean up commented code.

--- source/docq/integrations/slack/models.py
+++ source/docq/integrations/slack/models.py
@@ -1,46 +1,8 @@
 """Slack integration data models."""
 
-from typing import Optional, Self
+from typing import Optional
 
 from attr import dataclass
-
-# class SlackInstallation:
-#     """Slack application model."""
-
-#     def __init__(self: Self, data: tuple[str, str, str, int, Optional[int], str]) -> None:
-#         """Initialize."""
-#         self.app_id = data[0]
-#         self.team_id = data[1]
-#         self.team_name = data[2]
-#         self.org_id = data[3]
-#         self.space_group_id = data[4]
-#         self.created_at = data[5]
-
-# class SlackChannel:
-#     """Slack channel."""
-
-#     def __init__(self: Self, data: tuple[str, str, int, Optional[int], str]) -> None:
-#         """Initialize."""
-#         self.channel_id = data[0]
-#         self.channel_name = data[1]
-#         self.org_id = data[2]
-#         self.space_group_id = data[3]
-#         self.created_at = data[4]
-
-
-# class SlackMessage:
-#     """Slack message."""
-
-#     def __init__(self: Self, data: tuple) -> None:
-#         """Initialize."""
-#         self.client_msg_id = data[0]
-#         self.type = data[1]
-#         self.channel = data[2]
-#         self.team = data[3]
-#         self.user = data[4]
-#         self.text = data[5]
-#         self.ts = data[6]
-#         self.created_at = data[7]
 
 
 @dataclass
