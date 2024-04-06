8d235d990a17f50db1de72559ff0b4f3729102c1
Janaka Abeywardhana <contact@janaka.co.uk>
2024-04-06 16:53:40 +0100

    refactor(slack): switch to named env var pattern
    add null checks

--- source/docq/integrations/slack/slack_application.py
+++ source/docq/integrations/slack/slack_application.py
@@ -4,16 +4,21 @@ import os
 
 from docq.integrations.slack.slack_oauth_flow import SlackOAuthFlow
 from docq.support.store import get_sqlite_shared_system_file
+from grpc import StatusCode
+from h11 import ERROR
 from opentelemetry import trace
 from slack_bolt import App, BoltResponse
 from slack_bolt.oauth.callback_options import CallbackOptions, FailureArgs, SuccessArgs
 
+from ...config import ENV_VAR_DOCQ_SLACK_CLIENT_ID, ENV_VAR_DOCQ_SLACK_CLIENT_SECRET
+
 tracer = trace.get_tracer(__name__)
 
-CLIENT_ID = os.environ.get("SLACK_CLIENT_ID")
-CLIENT_SECRET = os.environ.get("SLACK_CLIENT_SECRET")
+CLIENT_ID = os.environ.get(ENV_VAR_DOCQ_SLACK_CLIENT_ID)
+CLIENT_SECRET = os.environ.get(ENV_VAR_DOCQ_SLACK_CLIENT_SECRET)
 SCOPES = ["app_mentions:read", "im:history", "chat:write", "channels:read", "groups:read", "im:read", "mpim:read"]
-USER_SCOPES = ["admin"]
+USER_SCOPES = []  # OAuth scopes to request if the bot needs to take actions on behalf of the user. Docq doesn't need to do this right now.
+
 
 @tracer.start_as_current_span(name="slack_success_callback")
 def success_callback(success_args: SuccessArgs) -> BoltResponse:
@@ -27,17 +32,23 @@ def failure_callback(failure_args: FailureArgs) -> BoltResponse:
     span.set_attribute("slack_failure_callback_args", str(failure_args))
     return failure_args.default.failure(failure_args)
 
-
-slack_app = App(
-    process_before_response=True,
-    oauth_flow=SlackOAuthFlow.sqlite3(
-        database=get_sqlite_shared_system_file(),
-        install_path="/api/integration/slack/v1/install",
-        redirect_uri_path="/api/integration/slack/v1/oauth_redirect",
-        client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET,
-        scopes=SCOPES,
-        user_scopes=USER_SCOPES,
-        callback_options=CallbackOptions(success=success_callback, failure=failure_callback),
-    ),
-)
+if CLIENT_ID and CLIENT_SECRET:
+    slack_app = App(
+        process_before_response=True,
+        oauth_flow=SlackOAuthFlow.sqlite3(
+            database=get_sqlite_shared_system_file(),
+            install_path="/api/integration/slack/v1/install",
+            redirect_uri_path="/api/integration/slack/v1/oauth_redirect",
+            client_id=CLIENT_ID,
+            client_secret=CLIENT_SECRET,
+            scopes=SCOPES,
+            user_scopes=USER_SCOPES,
+            callback_options=CallbackOptions(success=success_callback, failure=failure_callback),
+        ),
+    )
+else:
+    trace.get_current_span().record_exception(
+        ValueError("Slack client ID and client secret must be set in the environment.")
+    )
+    trace.Status(trace.StatusCode.ERROR)
+    raise ValueError("Slack client ID and client secret must be set in the environment.")
