1020aaf727841ff69fb431d9bd7ce0a34a68574d
Janaka Abeywardhana <contact@janaka.co.uk>
2024-05-18 14:53:11 +0100

    chore: adjust copy

--- web/admin/admin_integrations.py
+++ web/admin/admin_integrations.py
@@ -11,20 +11,20 @@ def admin_integrations_page() -> None:
     integrations = [
         {
             "name": "Slack",
-            "description": "Slack is a business communication platform that allows teams to communicate and collaborate.",
+            "description": "Slack the business communication platform that allows teams to communicate and collaborate.",
             "icon": "slack",
             "url": "/api/integration/slack/v1/install",
         },
         {
-            "name": "Teams",
-            "description": "Google Drive is a file storage and synchronization service developed by Google.",
+            "name": "MS Teams",
+            "description": "Mircosoft Teams the business communication platform that allows teams to communicate and collaborate.",
             "icon": "google-drive",
-            "url": "/api/integration/google-drive/v1/install",
+            "url": "/api/integration/msteams/v1/install",
         },
     ]
 
     integration = st.selectbox(
-        "Select an integration to get started",
+        "Select an integration",
         options=[integration["name"] for integration in integrations],
     )
 
