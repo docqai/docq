68a51bb0929da94cf2ff8e9178c238295f59a749
Janaka Abeywardhana <contact@janaka.co.uk>
2024-05-22 23:49:28 +0100

    test: fix

--- tests/unit/docq/support/llm_test.py
+++ tests/unit/docq/support/llm_test.py
@@ -1,10 +1,11 @@
 """Tests for docq.support.llm."""
 from unittest.mock import Mock, patch
 
-from docq.manage_assistants import Assistant
+from docq.domain import Assistant
 from docq.model_selection.main import LlmUsageSettings, LlmUsageSettingsCollection, ModelCapability
 from llama_index.core import ServiceContext
 from llama_index.core.chat_engine import SimpleChatEngine
+from llama_index.core.llms import ChatMessage, MessageRole
 
 
 #@patch("docq.support.metadata_extractors.DEFAULT_MODEL_PATH")
@@ -26,10 +27,15 @@ def test_run_chat() -> None:
         mocked_model_usage_settings = Mock(LlmUsageSettings)
         mocked_model_usage_settings.additional_args = {"arg1": "value1", "arg2": "value2"}
         mocked_model_usage_settings_collection.model_usage_settings = {ModelCapability.CHAT: mocked_model_usage_settings}
-        mocked_persona = Mock(Assistant)
-        mocked_persona.system_prompt_content= "Some system prompt"
-        mocked_persona.user_prompt_template_content = "My user prompt template"
+        mocked_assistant = Mock(Assistant)
+        mocked_assistant.system_message_content = "Some system prompt"
+        mocked_assistant.user_prompt_template_content = "My user prompt template"
 
-        response = run_chat("My ask", "My chat history", mocked_model_usage_settings_collection, mocked_persona)
+        response = run_chat(
+            "My ask",
+            [ChatMessage(role=MessageRole.USER, content="My chat history")],
+            mocked_model_usage_settings_collection,
+            mocked_assistant,
+        )
         mocked_chat.assert_called_once_with("My ask")
         assert response == "LLM response"
