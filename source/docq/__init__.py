1ad03b2f9e6ce42da259c21d8c0dd00b1745b613
Janaka Abeywardhana <contact@janaka.co.uk>
2024-09-07 23:23:29 +0100

    fix: incorrect docq version showing in UI

--- source/docq/__init__.py
+++ source/docq/__init__.py
@@ -1,24 +1,25 @@
 """Docq module."""
 import importlib.metadata as __metadata__
 
-_pkg_metadata = __metadata__.metadata(__package__).json
-project_urls = {}
-for item in _pkg_metadata["project_url"]:
-    key, value = item.split(", ")
-    project_urls[key] = value
+if __package__ is not None:
+    _pkg_metadata = __metadata__.metadata(__package__).json
+    project_urls = {}
+    for item in _pkg_metadata["project_url"]:
+        key, value = item.split(", ")
+        project_urls[key] = value
 
-
-# data from pyproject.toml
-__version__ = _pkg_metadata["version"] # version field
-__version_str__ = str(__version__)
-__summary__ = _pkg_metadata["summary"] # description field
-__description__ = _pkg_metadata["description"] # readme field
-__homepage_url__ = _pkg_metadata["home_page"] # homepage field
-__documentation_url__ = project_urls["Documentation"] # documentation field
-__repository_url__ = project_urls["Repository"] # repository field
-__author_email__ = _pkg_metadata["author_email"] # authors field
-__maintainer_email__ = _pkg_metadata["maintainer_email"] # maintainers field
-
+    # data from pyproject.toml
+    __version__ = _pkg_metadata["version"]  # version field
+    __version_str__ = str(__version__)
+    __summary__ = _pkg_metadata["summary"]  # description field
+    __description__ = _pkg_metadata["description"]  # readme field
+    __homepage_url__ = _pkg_metadata["home_page"]  # homepage field
+    __documentation_url__ = project_urls["Documentation"]  # documentation field
+    __repository_url__ = project_urls["Repository"]  # repository field
+    __author_email__ = _pkg_metadata["author_email"]  # authors field
+    __maintainer_email__ = _pkg_metadata["maintainer_email"]  # maintainers field
+else:
+    raise ValueError("Package name is not defined")
 
 __all__ = [
     "config",
